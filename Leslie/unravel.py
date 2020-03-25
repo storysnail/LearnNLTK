b4b9e95672859699e10a56cd41279eefccf8076d
elliot <elliot@YUi.yui.org>
2020-03-25 23:56:14 +0800

    uni

--- Leslie/unravel.py
+++ Leslie/unravel.py
@@ -4,11 +4,13 @@ import nltk
 import string
 import re
 import sys
+import numpy
 from sqlalchemy import create_engine
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords 
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import sentiwordnet as swn
+
 
 class usesql:
 
@@ -30,8 +32,42 @@ class usesql:
         except:
             print("Error: unable to fetch data")
         finally:
-            return result[0] #注意:result[0]是字符串，result则是包含一个字符串的list
-         
+            return result[0] #注意:result[0]是字符串，result则是包含一个字符串的元组
+
+    # 得到每条评论那些句子有情感词，把这些句子的id保存在结果中返回
+    def getSentenceIDs(self,CommentID):   
+        results = ''
+        SentenceIDs = [] 
+        try:
+            sql = 'select `1` from firstest where `0`=%d;'%(CommentID)
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            cursor.close()
+        except:
+            print("Error: unable to fetch data")
+        finally:
+            if(results):
+                for result in results:
+                    if (SentenceIDs.count(result[0]) == 0):
+                        SentenceIDs.append(result[0])
+ 
+            return SentenceIDs
+        
+    # 通过评论ID和句子ID得到对应情感词元组
+    def getSentiWords(self,CommentID,SentenceID):   
+        result = ''
+        try:
+            sql = 'select `2`,`4`,`5` from firstest where `0`=%d and `1`=%d;'%(CommentID,SentenceID)
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            cursor.close()
+        except:
+            print("Error: unable to fetch data")
+        finally:
+            return result
+        
 
     def output(self,result):    #将输出结果转为dataframe格式存入mysql，表的名称为测试阶段暂定，正式名称最后命名为reviewdata
         #这里使用了另一种数据库访问方法，按习惯是不应该混用两种数据库连接访问方法的
@@ -124,14 +160,14 @@ class prepare:
 
     #此处应有 打分
 
-    #遍历lemmas列表，将其中的形容词做情感词进行分析，之后将分析结果
-    #组成列表[还原后的词形,词性,pos_score,neg_score,obj_score]，
+    #遍历lemmas列表，将其中的所有形容词和副词做情感词进行分析，之后将分析结果
+    #组成列表[句子ID,还原后的词形,词性,pos_score,neg_score,obj_score],
     #再将这个列表加入senti_words列表
     def senti(self,cleanwords):
         senti_words = []
         for lemma in cleanwords:
-            #if(lemma[2] == wn.ADJ) or (lemma[2] == wn.ADV):
-            if(lemma[2] == wn.ADJ):
+            if(lemma[2] == wn.ADJ) or (lemma[2] == wn.ADV):
+            #if(lemma[2] == wn.ADJ):
                 SentiSynsets = swn.senti_synsets(lemma[1], lemma[2])
                 SentiSynset_list = list(SentiSynsets)
                 if(len(SentiSynset_list) > 0):
@@ -141,7 +177,7 @@ class prepare:
 
         return senti_words
 
-
+    #def senti_uni_method(self,cleanwords):
 
 class operate:
     
@@ -178,12 +214,13 @@ class operate:
 
 sql = usesql()  #先连接数据库，以免循环时重复连接
 op = operate()
-
+'''
+print("开始扫描数据提取情感词并存入数据库")
 i=1
 cleanreview = []
 Percentage = 0
 #total = 21000
-total = 210
+total = 21
 while i<total:
     #输出进度百分比
     print(str(round(i/total*100,1)) + '%')
@@ -196,6 +233,41 @@ while i<total:
     i=i+1
     #print(data)
 
-print('100%')
+print('100%,成功扫描情感词并存入数据库')
+print('------------------------------')
+'''
+print('从数据库中提取CommentID段评论每一句话的第一个情感词并计算其得分')
+CommentID = 3
+data = sql.getdata(CommentID)
+print(data)
+
+senti_words = op._main_(data)
+print(senti_words)
+
+#创建等比数列 2^0,2^1,2^2,2^3 .. 2^99共100个，应该够用了吧
+pro_series = numpy.logspace(0,99,100,base=2)
+
+SentenceIDs = sql.getSentenceIDs(CommentID)
+for SentenceID in SentenceIDs:
+    Weights = 0
+    score = 0
+    SentiWords = sql.getSentiWords(CommentID,SentenceID)
+    if(SentiWords):
+        FirstSentiWord = SentiWords[0][0] #得到第一个情感词
+        print('------------------------------')
+        print("开始计算  [%s]"%(FirstSentiWord))
+        for SentiWord in SentiWords:
+            if(SentiWord[0] == FirstSentiWord): #只计算第一个情感词
+                print("%.8f = (%.8f - %.8f) * (1 / %.8f)"%((SentiWord[1] - SentiWord[2]) * (1/pro_series[Weights]),SentiWord[1],SentiWord[2],pro_series[Weights]))
+                score = score + (SentiWord[1] - SentiWord[2]) * (1/pro_series[Weights])
+                print("第%d次求和结果%.8f"%(Weights,score))
+                Weights = Weights + 1
+
+        #通过求和，我们得到第一段评论第一句话第一个情感词的综合得分
+        print("%s -- score:[%.8f]"%(FirstSentiWord,score))
+        print('------------------------------')
+
+
+
 sql.sqlQuit()
 
